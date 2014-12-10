05049a343135327ab5602912c539e3ce67e95901
Aaron Coburn <acoburn@amherst.edu>
2014-12-10 12:44:32 -0500

    added support for dynamic transform endpoints

--- src/test/java/org/fcrepo/camel/FedoraTransformTest.java
+++ src/test/java/org/fcrepo/camel/FedoraTransformTest.java
@@ -18,6 +18,7 @@ package org.fcrepo.camel;
 import static org.apache.camel.Exchange.HTTP_METHOD;
 import static org.apache.camel.Exchange.CONTENT_TYPE;
 import static org.fcrepo.camel.FedoraEndpoint.FCREPO_IDENTIFIER;
+import static org.fcrepo.camel.FedoraEndpoint.FCREPO_TRANSFORM;
 import static org.fcrepo.camel.integration.FedoraTestUtils.getFcrepoBaseUrl;
 import static org.fcrepo.camel.integration.FedoraTestUtils.getFcrepoEndpointUri;
 import static org.fcrepo.camel.integration.FedoraTestUtils.getTurtleDocument;
@@ -73,6 +74,7 @@ public class FedoraTransformTest extends CamelTestSupport {
             "id      = . :: xsd:string ;\n" +
             "title = dc:title :: xsd:string;\n" +
             "uuid = fcrepo:uuid :: xsd:string;";
+
         headers.clear();
         headers.put(FCREPO_IDENTIFIER, identifier);
         headers.put(CONTENT_TYPE, "application/rdf+ldpath");
@@ -84,6 +86,22 @@ public class FedoraTransformTest extends CamelTestSupport {
         headers.put(HTTP_METHOD, "GET");
         template.sendBodyAndHeaders("direct:get", null, headers);
 
+        headers.clear();
+        headers.put(FCREPO_IDENTIFIER, identifier);
+        headers.put(FCREPO_TRANSFORM, "default");
+        headers.put(HTTP_METHOD, "GET");
+        template.sendBodyAndHeaders("direct:get2", null, headers);
+
+        headers.clear();
+        headers.put(FCREPO_IDENTIFIER, identifier);
+        headers.put(FCREPO_TRANSFORM, "default");
+        template.sendBodyAndHeaders("direct:get2", null, headers);
+
+        headers.clear();
+        headers.put(FCREPO_IDENTIFIER, identifier);
+        headers.put(FCREPO_TRANSFORM, "");
+        template.sendBodyAndHeaders("direct:get", null, headers);
+
 
         // Teardown
         final Map<String, Object> teardownHeaders = new HashMap<>();
@@ -91,9 +109,8 @@ public class FedoraTransformTest extends CamelTestSupport {
         teardownHeaders.put(FCREPO_IDENTIFIER, identifier);
         template.sendBodyAndHeaders("direct:teardown", null, teardownHeaders);
 
-
         // Assertions
-        resultEndpoint.expectedMessageCount(3);
+        resultEndpoint.expectedMessageCount(6);
         resultEndpoint.expectedHeaderReceived("Content-Type", "application/json");
         resultEndpoint.assertIsSatisfied();
     }
@@ -115,6 +132,11 @@ public class FedoraTransformTest extends CamelTestSupport {
                 from("direct:get")
                     .to(fcrepo_uri + "?transform=default")
                     .to("mock:result");
+
+                from("direct:get2")
+                    .to(fcrepo_uri + "?transform=foo")
+                    .to("mock:result");
+
 
                 from("direct:post")
                     .to(fcrepo_uri + "?transform=true")
